47c08a49db134f6ab75910081f9b0225f156cd04
Kresimir <kresimir.maric85@gmail.com>
2018-01-03 21:14:48 +0100

    basic funcionality added

--- calculator.js
+++ calculator.js
@@ -2,43 +2,89 @@
     console.log("js file loaded");
     
     var kilowats;
+    var horsePower;
     var multiplicator;
 
-    (function attachHandlers() {
-        var form = document.getElementById("calculatorForm");
-        $(form).submit(submitAndProcessForm);
-    })();
+    function validateNumbers() {
+        var valid = true;
+        var kw = $("#kw").val();
+        var ps = $("#ps").val();
 
-    function validateFormInputs() {
-        console.log("form submitted");
-        return true;
+        console.log("kilowats: ", kw, " horsePower: ", ps, " multiplicator: ", multiplicator);
+
+        if($.isNumeric(kw)) {
+            kilowats = kw;
+        } else {
+            valid = false;   
+        }
+        if($.isNumeric(ps)){
+            horsePower = ps;
+        } else {
+            valid = false;
+        }
+        
+        return valid;
+    }  
+
+    function validateFormInputs() {        
+        return validateNumbers() && multiplicator !== undefined;
     }
 
-    function processForm(form) {
-        console.log(form);
-
-
+    function selectHandler() {
+        
+        multiplicator = $("#optionSelect").val();
+        console.log(multiplicator);
     }
 
     function onSuccess() {
-
-
-        calculate();
+        calculate();                
     }
 
-    function calculate(kw, multiplicator) {
-        console.log("kilowats " + kw, "multiplicator " + multiplicator);
+    function calculate() {
+        console.log("kilowats " + kilowats, " horsePower: ", horsePower,  "multiplicator " + multiplicator);
+        var result = kilowats * horsePower * multiplicator
+        displayResult(result, multiplicator);
     }
 
-    function submitAndProcessForm() {
-        
-        console.log("submitAndProcessForm");
+    function displayResult(result, rate) {
+        $("#amount").val(result);
 
-        processForm(form);
-        
+        var text;
+
+        switch(rate) {
+            case "1":
+                text = "per year";
+                break;
+            case "1.1":
+                text = "per half year";
+                break;
+            case "1.2":
+                text = "";
+                break;
+            default:
+                text = "per month";
+                break;
+        }
+
+        $(".result-appendix").html(text);
+        $("#result").show();
+    }
+
+    function submitAndProcessForm() {        
+        console.log("submitAndProcessForm", validateFormInputs());
+       
         return validateFormInputs() ? onSuccess() : false;
     }    
 
-    
+    $(document).ready(function() {        
+        $("#calculatorForm").on("submit", function(event){    
+            submitAndProcessForm();        
+            return false;
+        });
+
+        $("#optionSelect").on("change", selectHandler).trigger("change");        
+        $(".number").on("blur", validateNumbers);       
+        // $("#result").hide(); 
+    });
 
 })(jQuery);
