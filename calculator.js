a03bf9e04089ccfec54323c279020a7bbff2a25f
Kresimir <kresimir.maric85@gmail.com>
2018-01-04 13:27:24 +0100

    Calculation changed

--- calculator.js
+++ calculator.js
@@ -42,11 +42,11 @@
     }  
 
     function fromKwToHp(kw) {
-        return kw * 1.34102209;
+        return round(kw * 1.35962, 5);
     }
 
     function fromHpToKw(hp) {
-        return hp * 0.745699872;
+        return round(hp * 0.7355, 5);
     }
 
     function validateFormInputs() {        
@@ -61,41 +61,74 @@
         calculate();                
     }
 
-    function getPriceByJKw(kw) {        
-            if(kw < 25) 
-                return 0;
-            else if(kw < 90.99)
-                return kw * 0.62;
-            else if(kw < 110.99)
-                return kw * 0.66;
-            else
-                return kw * 0.75;
+    function getPriceByKw(kw) {  
+        var sum = 0.00;
+
+        if(kw <= 24.99) 
+            return sum;
+        
+        if(kw <= 66.99) {
+            return round((kw - 24) * 0.682, 2);
+        }            
+        
+        if(kw <= 97.99) {
+            sum = round(41 * 0.682, 2);         //previous tax grade in full
+            sum = round((kw - 67) * 0.726, 2);  //current tax grade
+        }            
+        else {
+            sum += round(41 * 0.682, 2);           //previous tax grade in full
+            sum += round(30 * 0.726, 2);           //previous tax grade in full
+            sum += round((kw - 98) * 0.825, 2);    //current tax grade
+        }          
+
+        return sum;
     }
 
     function calculate() {
-        var price = getPriceByJKw(kilowats);
-        console.log('price', price);
+        var price = getPriceByKw(kilowats);
 
-        var result = price * parseTextToNumber(multiplicator);
-        result = (Math.round(result * 100) / 100).toFixed(2);
+        var resultPerYear = round(price * 12, 2);
+        var divider = parseTextToNumber(multiplicator);
+        var frequency = getFrequencyDivider(divider);        
+
+        var result = round(round(resultPerYear / frequency, 2) / divider, 2);
+        result = round(result, 2)
         displayResult(result, multiplicator);
+    }
+
+    function getFrequencyDivider(divider) {
+        switch(divider) {
+            case 1.1:
+                return 1;
+            case 1.08:
+                return 4;
+            case 1.06:
+                return 2;
+            default:
+                return 12;
+        }
+    }
+
+    function round(number, decimals) {
+        var rounded = (Math.round(number * 100) / 100).toFixed(decimals);
+        return parseTextToNumber(rounded);
     }
 
     function displayResult(result, rate) {
         var text;
 
         switch(rate) {
-            case "1":
-                text = "per year";
+            case "1.1":
+                text = "pro Jahr";
                 break;
             case "1.06":
-                text = "per half year";
+                text = "pro Halbjahr";
                 break;
             case "1.08":
-                text = "per quarter";
+                text = "pro Quartal";
                 break;
             default:
-                text = "per month";
+                text = "pro Monat";
                 break;
         }
 
